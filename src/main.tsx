762defa010e59251ef73e4dd5bf68e64dbc4f751
nasubi916 <r916nis@gmail.com>
2024-03-23 15:09:15 +0900

    ✨ init commit !!

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import { Routes } from '@generouted/react-router'
+
+createRoot(document.getElementById('root')!).render(<Routes />)
+
+
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import generouted from '@generouted/react-router/plugin'
+
+export default defineConfig({ plugins: [react(), generouted()] })
