90d82ae905b15f3c95f1557f942e4dc0affe901a
nasubi916 <r916nis@gmail.com>
2024-03-27 23:46:30 +0900

    close Topに小洒落た一文を加える #31 小洒落てないかもしれない

--- src/components/sections/Top.tsx
+++ src/components/sections/Top.tsx
@@ -1,16 +1,36 @@
 import { type ReactElement } from "react";
-import { Image } from "@mantine/core";
+import { Image, em } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { getImage } from "../../utils";
+import { $isMobile } from "../../stores/option";
 import { styled as p } from "../../../styled-system/jsx";
 
 export default function Top(): ReactElement {
+  $isMobile.set(useMediaQuery(`(max-width: ${em(750)})`) ?? false);
+  const isMobile = $isMobile.value ?? false;
   return (
-    <p.div style={{ height: "calc(100vh - 50px)" }}>
+    <p.div position="relative" style={{ height: "calc(100vh - 50px)" }}>
       <Image
         fallbackSrc="https://placehold.co/600x400?text=Placeholder"
         h="100%"
         src={getImage("picture")}
       />
+      <p.div
+        bottom={20}
+        color="white"
+        fontFamily="Noto serif JP"
+        fontSize={isMobile ? 30 : 80}
+        left={50}
+        position="absolute"
+        textAlign="center"
+      >
+        言葉はまだ行いじゃない
+        {/*
+        他人を蹴落とし合って低め合うのがインターネット
+        成し遂げたければ､命ではなく生涯を
+        思ったことをすぐ口に出すのは美徳ではない
+         */}
+      </p.div>
     </p.div>
   );
 }
